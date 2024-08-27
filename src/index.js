9e1628a4e39fea4fe4fc53adfa35efc37390f0c6
Pete Buzzell <pete@Petes-Laptop.local>
2024-08-27 16:55:26 -0400

    another vercel fix attempt

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
+
+try {
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+} catch (error) {
+  console.error("Error rendering the app:", error);
+  document.body.innerHTML = `<div>Error rendering the app: ${error.message}</div>`;
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
