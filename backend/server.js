586c89fd0bfa6153ca0f1639c375c71d3111f3e2
Pete Buzzell <pete@Petes-Laptop.local>
2024-08-28 13:20:26 -0400

    more vercel deployment fixes

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,15 @@ const app = express();
 app.use('/api', require('./routes/api'));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-module.exports = app; // For Vercel
+module.exports = app;
